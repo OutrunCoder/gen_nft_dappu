23d05ae77c6301f62d0e8d84135668e930b1af53
David Pineda <david.pineda.orvc@gmail.com>
2023-02-23 22:06:29 -0500

    set and test of COST for nfts

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -6,11 +6,19 @@ import "./ERC721Enumerable.sol";
 struct NFTDeploymentArgs {
   string _name;
   string _symbol;
+  uint256 _cost;
 }
 
 contract NFT is ERC721Enumerable {
+    uint256 public cost;
 
-    constructor(NFTDeploymentArgs memory args) ERC721(args._name, args._symbol) {
-
+    constructor(NFTDeploymentArgs memory args)
+    ERC721(
+        args._name,
+        args._symbol
+    ) {
+        cost = args._cost;
     }
 }
+
+// @ 14:50 "extending out of the box"
--- test/NFT.js
+++ test/NFT.js
@@ -1,28 +1,39 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
+const tokensToWei = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether');
+}
+
+const etherToWei = tokensToWei;
+
 describe('NFT', () => {
   const NAME = 'Dapp Punks';
   const SYMBOL = 'DP';
+  const COST = etherToWei(10);
 
   let nftContract;
 
-  beforeEach(async () => {
-    const NFT_factory = await ethers.getContractFactory('NFT');
-    nftContract = await NFT_factory.deploy({
-      _name: NAME,
-      _symbol: SYMBOL
-  });
-  })
-
+  
   describe('Deployment', () => {
+    beforeEach(async () => {
+      const NFT_factory = await ethers.getContractFactory('NFT');
+      nftContract = await NFT_factory.deploy({
+          _name: NAME,
+          _symbol: SYMBOL,
+          _cost: COST
+      });
+    })
 
     it('has correct name', async () => {
-      expect(await nftContract.name()).to.equal(NAME)
-    })
+      expect(await nftContract.name()).to.equal(NAME);
+    });
     it('has correct symbol', async () => {
-      expect(await nftContract.symbol()).to.equal(SYMBOL)
-    })
+      expect(await nftContract.symbol()).to.equal(SYMBOL);
+    });
+    it('returns the cost to mint', async () => {
+      expect(await nftContract.cost()).to.equal(COST);
+    });
   })
 
 })
