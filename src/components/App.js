af2de143fc5b6a494a5d95b343106138a334e9a3
David Pineda <david.pineda.orvc@gmail.com>
2023-02-25 21:47:24 -0500

    setup and collect contract data management

--- src/components/App.js
+++ src/components/App.js
@@ -7,30 +7,56 @@ import Navigation from './Navigation';
 import Loading from './Loading';
 
 // ABIs: Import your contract ABIs here
-// import TOKEN_ABI from '../abis/Token.json'
+import NFT_ABI from '../abis/NFT.json'
 
 // Config: Import your network config here
-// import config from '../config.json';
+import config from '../config.json';
 
 function App() {
-  const [account, setAccount] = useState(null)
-  const [balance, setBalance] = useState(0)
+  const [provider, setProvider] = useState(null);
+  const [nftContract, setNftContract] = useState(null);
 
-  const [isLoading, setIsLoading] = useState(true)
+  const [account, setAccount] = useState(null);
+  const [revealtime, setRevealTime] = useState(0);
+  const [ maxSupply, setMaxSupply] = useState(0);
+  const [ totalSupply, setTotalSupply] = useState(0);
+  const [ cost, setCost] = useState(0);
+  const [ balance, setBalance] = useState(0);
+
+  const [isLoading, setIsLoading] = useState(true);
 
   const loadBlockchainData = async () => {
+    // const network = 
+    const { nft } = config[31337];
+
     // Initiate provider
     const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider);
+
+    // initialize contracts
+    const nftContract = new ethers.Contract(nft.address, NFT_ABI, provider);
+    setNftContract(nftContract);
 
     // Fetch accounts
     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
     const account = ethers.utils.getAddress(accounts[0])
     setAccount(account)
 
-    // Fetch account balance
-    let balance = await provider.getBalance(account)
-    balance = ethers.utils.formatUnits(balance, 18)
-    setBalance(balance)
+    // when minting is allowd - fetch countdown
+    const publicMintOpenOn = await nftContract.publicMintOpenOn();
+    setRevealTime(publicMintOpenOn.toString() + '000');
+
+    // max supply
+    setMaxSupply(await nftContract.maxSupply());
+    
+    // total supply
+    setTotalSupply(await nftContract.totalSupply());
+    
+    // cost
+    setCost(await nftContract.cost());
+    
+    // balance of current account
+    setBalance(await nftContract.balanceOf(account));
 
     setIsLoading(false)
   }
@@ -51,7 +77,6 @@ function App() {
         <Loading />
       ) : (
         <>
-          <p className='text-center'><strong>Your ETH Balance:</strong> {balance} ETH</p>
           <p className='text-center'>Edit App.js to add your code here.</p>
         </>
       )}
